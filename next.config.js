5157b2e25ee3dc6023e713f14fd60ed1ea725e98
mistalyon <kemaloglu100@gmail.com>
2026-03-14 10:02:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-      remotePatterns: [
+    images: {
+          remotePatterns: [
             {
-                    protocol: 'https',
-                            hostname: 'pilatestopu.com',
-                                  },
-                                        {
-                                                protocol: 'https',
-                                                        hostname: 'www.pilatestopu.com',
-                                                              },
-                                                                    {
-                                                                            protocol: 'https',
-                                                                                    hostname: '*.supabase.co',
-                                                                                          },
-                                                                                              ],
-                                                                                                },
-                                                                                                  async redirects() {
-                                                                                                      return [];
-                                                                                                        },
-                                                                                                        };
-                                                                                                        
-                                                                                                        module.exports = nextConfig; */
+                      protocol: 'https',
+                      hostname: 'pilatestopu.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'www.pilatestopu.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: '*.supabase.co',
+            },
+                ],
+    },
+    async redirects() {
+          return [];
+    },
+};
+
+module.exports = nextConfig;
