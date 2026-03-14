42dda6f8014b5a4acfef5abbf34e0f438bc54e08
mistalyon <kemaloglu100@gmail.com>
2026-03-14 09:16:23 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import HeroSection from '@/components/home/HeroSection';
+import PilatesTypesSection from '@/components/home/PilatesTypesSection';
+import FeaturedStudios from '@/components/home/FeaturedStudios';
+import TestimonialsSection from '@/components/home/TestimonialsSection';
+import PopularCitiesSection from '@/components/home/PopularCitiesSection';
+import BlogPreviewSection from '@/components/home/BlogPreviewSection';
+import CTASection from '@/components/home/CTASection';
+import SeoContentSection from '@/components/home/SeoContentSection';
+import JsonLd from '@/components/seo/JsonLd';
+
+export default function Home() {
+  const jsonLd = {
+      '@context': 'https://schema.org',
+          '@type': 'WebSite',
+              name: 'PilatesTopu.com',
+                  url: 'https://pilatestopu.com',
+                      description: "Türkiye'nin en kapsamlı pilates rehberi",
+                          potentialAction: {
+                                '@type': 'SearchAction',
+                                      target: 'https://pilatestopu.com/ara?q={search_term_string}',
+                                            'query-input': 'required name=search_term_string',
+                                                },
+                                                  };
+
+                                                    return (
+                                                        <>
+                                                              <JsonLd data={jsonLd} />
+                                                                    <HeroSection />
+                                                                          <PilatesTypesSection />
+                                                                                <FeaturedStudios />
+                                                                                      <TestimonialsSection />
+                                                                                            <PopularCitiesSection />
+                                                                                                  <BlogPreviewSection />
+                                                                                                        <CTASection />
+                                                                                                              <SeoContentSection />
+                                                                                                                  </>
+                                                                                                                    );
+                                                                                                                    }
