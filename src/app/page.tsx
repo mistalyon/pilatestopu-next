16587e875bda07cbafffea278757da2533cb1bae
mistalyon <kemaloglu100@gmail.com>
2026-03-14 17:04:05 +0300

    faz4: Schema.org JSON-LD - WebSite, Organization, Breadcrumb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,13 @@ import PopularCitiesSection from '@/components/home/PopularCitiesSection';
 import BlogPreviewSection from '@/components/home/BlogPreviewSection';
 import CTASection from '@/components/home/CTASection';
 import SeoContentSection from '@/components/home/SeoContentSection';
-import JsonLd from '@/components/seo/JsonLd';
 
 const websiteJsonLd = {
   '@context': 'https://schema.org',
   '@type': 'WebSite',
   name: 'PilatesTopu.com',
   url: 'https://pilatestopu.com',
-  description: 'Turkiye\'nin en kapsamli pilates rehberi',
+  description: 'Türkiye\'nin en kapsamlı pilates rehberi. Salonlar, dersler ve ekipmanlar.',
   potentialAction: {
     '@type': 'SearchAction',
     target: 'https://pilatestopu.com/ara?q={search_term_string}',
@@ -21,10 +20,57 @@ const websiteJsonLd = {
   },
 };
 
+const organizationJsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  name: 'PilatesTopu',
+  url: 'https://pilatestopu.com',
+  logo: 'https://www.pilatestopu.com/wp-content/uploads/2025/04/Pilates-Topu.png',
+  contactPoint: {
+    '@type': 'ContactPoint',
+    telephone: '+90-544-673-22-02',
+    contactType: 'customer service',
+    areaServed: 'TR',
+    availableLanguage: 'Turkish',
+  },
+  address: {
+    '@type': 'PostalAddress',
+    streetAddress: 'Balat',
+    addressLocality: 'Fatih',
+    addressRegion: 'İstanbul',
+    addressCountry: 'TR',
+  },
+  sameAs: [],
+};
+
+const breadcrumbJsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  itemListElement: [
+    {
+      '@type': 'ListItem',
+      position: 1,
+      name: 'Anasayfa',
+      item: 'https://pilatestopu.com',
+    },
+  ],
+};
+
 export default function Home() {
   return (
     <>
-      <JsonLd data={websiteJsonLd} />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJsonLd) }}
+      />
       <HeroSection />
       <PilatesTypesSection />
       <FeaturedStudios />
