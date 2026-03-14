d083493d9489c07169392498d45f2ef6657cd67f
mistalyon <kemaloglu100@gmail.com>
2026-03-14 17:15:14 +0300

    faz4: robots.ts with sitemap reference

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,14 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/admin/"],
-    },
-    sitemap: "https://pilatestopu-next.vercel.app/sitemap.xml",
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
+      },
+    ],
+    sitemap: 'https://pilatestopu-next.vercel.app/sitemap.xml',
   };
 }
