65668c4aa253b8225f6900993233822576c1cdce
mistalyon <kemaloglu100@gmail.com>
2026-03-17 20:33:59 +0300

    fix: robots.ts sitemap URL to www.pilatestopu.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/_next/'],
       },
     ],
-    sitemap: 'https://pilatestopu.com/sitemap.xml',
+    sitemap: 'https://www.pilatestopu.com/sitemap.xml',
   };
 }
