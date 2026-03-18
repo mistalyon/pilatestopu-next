080ca8a6ea0e282071e50378b76a6eb924975ae7
mistalyon <kemaloglu100@gmail.com>
2026-03-19 00:16:34 +0300

    Update next.config.js: /p-c/istanbul-pilates -> /p-c/istanbul

--- next.config.js
+++ next.config.js
@@ -1,27 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.pilatestopu.com",
-      },
-      {
-        protocol: "https",
-        hostname: "pilatestopu.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cwcmgdfsozipuoiidmdf.supabase.co",
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    images: {
+          remotePatterns: [
+            {
+                      protocol: "https",
+                      hostname: "www.pilatestopu.com",
+            },
+            {
+                      protocol: "https",
+                      hostname: "pilatestopu.com",
+            },
+            {
+                      protocol: "https",
+                      hostname: "cwcmgdfsozipuoiidmdf.supabase.co",
+            },
+                ],
+    },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
+    async redirects() {
+          return [
+            {
+                      source: '/p-c/istanbul-pilates',
+                      destination: '/p-c/istanbul',
+                      permanent: true,
+            },
+                ];
+    },
 };
 
 module.exports = nextConfig;
